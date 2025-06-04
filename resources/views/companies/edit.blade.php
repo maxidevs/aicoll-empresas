abb9ac8c4df6e1d1f7eae690aa745eddb49732c5
Max Jimenez <maxjip@gmail.com>
2025-06-04 04:42:33 -0500

    feat: update company edit form to require fields and fix script inclusion

--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -6,8 +6,6 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-
-  <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
   @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
     @vite(['resources/css/app.css', 'resources/js/app.js'])
   @endif
@@ -22,28 +20,27 @@
         <h3 class="mb-0">Editar Empresa</h3>
       </div>
       <div class="card-body">
-        <form id="form-edit" action="{{ route('companies.update', $company) }}" method="POST">
+        <form id="form-store" action="{{ route('companies.update', $company) }}" method="POST">
           @csrf
           <input type="hidden" name="_method" value="PUT">
           <div class="form-group">
             <label for="name">Nombre</label>
-            <input type="text" class="form-control" id="name" name="name" value="{{ $company->name }}">
+            <input type="text" required class="form-control" id="name" name="name"
+              value="{{ $company->name }}">
           </div>
           <div class="form-group">
             <label for="address">Dirección</label>
-            <input type="text" class="form-control" id="address" name="address" value="{{ $company->address }}">
+            <input type="text" required class="form-control" id="address" name="address"
+              value="{{ $company->address }}">
           </div>
           <div class="form-group">
             <label for="phone">Teléfono</label>
-            <input type="text" class="form-control" id="phone" name="phone" value="{{ $company->phone }}">
-          </div>
-          <div class="form-group">
-            <label for="nit">NIT</label>
-            <input type="text" class="form-control" id="nit" name="nit" value="{{ $company->nit }}">
+            <input type="text" required class="form-control" id="phone" name="phone"
+              value="{{ $company->phone }}">
           </div>
           <div class="form-group">
             <label for="active">Estado</label>
-            <select class="form-control" id="active" name="active">
+            <select class="form-control" id="active" name="active" required>
               <option value="1" {{ $company->active ? 'selected' : '' }}>Activa</option>
               <option value="0" {{ $company->active ? '' : 'selected' }}>Inactiva</option>
             </select>
@@ -62,6 +59,7 @@
   </footer>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"
     integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
 </body>
 
 </html>
