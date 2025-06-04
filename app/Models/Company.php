2f26ba5ead2ca40c454b6f7d7ca2ef89ed974f7c
Max Jimenez <maxjip@gmail.com>
2025-06-04 01:11:43 -0500

    feat: add CompaniesSeeder to populate database with Company model instances

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Company extends Model
 {
+  use HasFactory;
   protected $fillable = ['name', 'address', 'phone', 'nit', 'active'];
 }
