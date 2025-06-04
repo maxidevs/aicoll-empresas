4de070ef4371fed35985386621eb19956426d8e5
Max Jimenez <maxjip@gmail.com>
2025-06-04 01:11:21 -0500

    feat: rename CompanyFactory

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Company>
  */
-class CompaniesFactory extends Factory
+class CompanyFactory extends Factory
 {
   /**
    * Define the model's default state.
