0169c731c85f9f49a344d2b106613c30e70e6083
Max Jimenez <maxjip@gmail.com>
2025-06-04 01:03:24 -0500

    feat:  Add CompanyController resource and Company model with migration and factory

---
+++ database/factories/CompaniesFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Company>
+ */
+class CompaniesFactory extends Factory
+{
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      'name' => $this->faker->name(),
+      'address' => $this->faker->address(),
+      'phone' => $this->faker->phoneNumber(),
+      'nit' => $this->faker->unique()->randomNumber(9),
+      'active' => true,
+    ];
+  }
+}
